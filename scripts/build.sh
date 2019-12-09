1a2a69a011e8a9dae3c8e676e3e87f5d0eeaa49e
LUIS NIETO GAMIZ <48691923a@AD.FPMISLATA.COM>
2019-12-09 09:42:53 +0100

    feat(#0):Inicio proyecto

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf ./dist/
+mkdir ./dist/
+cp -r ./src/* ./dist/
---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf C:/xampp/htdocs/api
+mkdir C:/xampp/htdocs/api
+cp -r ./dist/* C:/xampp/htdocs/api
